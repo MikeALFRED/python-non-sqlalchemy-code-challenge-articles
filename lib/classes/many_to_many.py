f7d6137c310b5e2c25fcb51d49bff539d0ab0e64
Paps <Mikealfredmuigai@gmail.com>
2024-05-27 22:47:11 +0300

    intial commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,118 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
+from collections import defaultdict
+
 class Author:
     def __init__(self, name):
-        self.name = name
+      
+        self._name = name
+        self._articles=[]
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if hasattr(self, 'name') and self._name is not None:
+             return
+        elif type(new_name) is not str or len(new_name) == 0:
+            return
+        self._name = new_name
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
-        pass
+        new_article = Article(self, magazine, title)
+        self._articles.append(new_article)
+        return new_article
 
     def topic_areas(self):
-        pass
+        if not self.articles():
+            return None
+        return list(set(article.magazine.category for article in self.articles()))
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        
+        self._name = name
+        self._category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if type(new_name) is str and 2 <= len(new_name) <= 16: 
+             self._name = new_name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if  isinstance(new_category, str) and len(new_category) > 0:      
+             self._category = new_category
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in Article.all if article.magazine == self]
+        return titles if titles else None
 
+    
     def contributing_authors(self):
-        pass
+        articles_per_author = defaultdict(int)
+        for article in self.articles():
+            articles_per_author[article.author] += 1
+        contributing_authors = [author for author, count in articles_per_author.items() if count > 2]
+        return contributing_authors if contributing_authors else None
+    
+    
+class Article:
+    all = []
+
+    def __init__(self, author, magazine, title):
+        
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self, new_title):
+        if type (new_title) is not str or len(new_title) == 0: 
+           return 
+        self._title = new_title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, new_author):
+        if isinstance(new_author, Author):
+           
+            self._author = new_author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if  isinstance(new_magazine, Magazine):
+           
+            self._magazine = new_magazine
